2956872cb088e0ed93a4958fe8d4eacf41296f25
Armando <armandocandelasalvarado@gmail.com>
2024-10-03 12:54:50 -0600

    configuración inicial

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const express = require('express');
-const app = express();
-const logger = require('morgan');
+const express = require('express'); //servidor web
+const app = express(); 
+const logger = require('morgan'); //logger
 require('dotenv').config();
-const PORT = process.env.PORT;
-const conn = require('mysql2');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const PORT = process.env.PORT; //Puerto de la app
+const conn = require('mysql2'); //Para usar BD
+const bodyParser = require('body-parser'); //Ordenar información
+const cors = require('cors'); //Permitir peticiones
+
+//Conectar a la base de datos
+const conexion = conn.createPool({
+    host:process.env.DB_HOST,
+    host:process.env.DB_USER,
+    host:process.env.DB_PASSWORD,
+    host:process.env.DB_NAME,
+    host:process.env.DB_PORT,
+});
+
+//Configurar servidor API
+app.use(cors());
+app.use(logger('dev')); //dev para debuggear para cuando estemos en modo desarrollo
+app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//Crear endpoints 
+app.get('*', (req, res)=>{
+    res.send({message:'Ruta no válida'});
+});
+
+//Verificar la conexión a la DB
+conexion.getConnection((error)=>{
+    if(error){
+        console.log("No se pudo conectar a la DB");
+    } else{
+        console.log("Conectado a la DB");
+        app.listen(PORT, ()=>{
+            console.log("Servidor API funcionando")
+        });
+    }
+});
